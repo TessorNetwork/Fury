4deaa909f2c2758c1d05edf2c8bad867fe4311dc
nimrostafarian <adrian2ross@gmail.com>
2023-01-17 06:09:58 +0530

    commit

--- container_exec.sh
+++ container_exec.sh
@@ -27,13 +27,13 @@ FYD_FLAGS="--home=$CHAIN_DIR $FYD_EXTRA_FLAGS"
 FYD_START_FLAGS="$FYD_START_FLAGS"
 if [ -z "$(ls -A $CHAIN_DIR)" ]; then
 	# chain directory empty, let's build a new chain
-	./commercionetworkd unsafe-reset-all $FYD_FLAGS
-	./commercionetworkd init $NODENAME $FYD_FLAGS
+	./furydd unsafe-reset-all $FYD_FLAGS
+	./furydd init $NODENAME $FYD_FLAGS
 	cp $GENESIS_DIR/genesis.json $CHAIN_DIR/config/genesis.json
 	sed -e "s|persistent_peers = \".*\"|persistent_peers = \"$(cat $GENESIS_DIR/.data | grep -oP 'Persistent peers\s+\K\S+')\"|g" $CHAIN_DIR/config/config.toml > $CHAIN_DIR/config/config.toml.tmp
 	mv $CHAIN_DIR/config/config.toml.tmp  $CHAIN_DIR/config/config.toml
 fi
 
-./commercionetworkd start $FYD_FLAGS $FYD_START_FLAGS &
+./furydd start $FYD_FLAGS $FYD_START_FLAGS &
 sleep 3 # let cnd start first before running cncli rest server
-./commercionetworkd rest-server --chain-id=$CHAINID --laddr $FYCLI_LISTEN_ADDR 
+./furydd rest-server --chain-id=$CHAINID --laddr $FYCLI_LISTEN_ADDR 
